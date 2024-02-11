30f11d03ffb68ee8ced1ba0932df2d0bcb9c8456
Yel18 <108323670+Yel18@users.noreply.github.com>
2024-02-11 13:59:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const heart = document.getElementById('heart');
 const message = document.getElementById('message');
 
 button.addEventListener('click', () => {
-  heart.src = "https://media1.tenor.com/m/OqW4q0evlmgAAAAC/frog-frog-love.gif";
+  heart.src = "https://media1.tenor.com/m/XZGm-kKjguMAAAAC/kawaii-bunny.gif";
   message.innerText = "Yay!!!";
+  button.style.display = 'none';
+  noButton.style.display = 'none';
 });
 
 noButton.addEventListener('click', () => {
