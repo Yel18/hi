777c5d4a9c89d77953618969b57fb22de39f7054
Yel18 <108323670+Yel18@users.noreply.github.com>
2024-02-11 11:10:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const button = document.getElementById("valentine-button");
 const responseContainer = document.getElementById("response-container");
 const yesButton = document.getElementById("yes-button");
 const noButton = document.getElementById("no-button");
+const imageContainer = document.getElementById("image-container");
+const image = document.getElementById("image");
 
 button.addEventListener("click", function() {
   responseContainer.style.display = "block";
+  imageContainer.style.display = "block";
 });
 
 yesButton.addEventListener("click", function() {
   responseContainer.style.display = "none";
   button.textContent = "Yes!";
-  
+
+  // Remove the event listener for the "click" event
+  yesButton.removeEventListener("click", arguments.callee);
 });
 
 noButton.addEventListener("click", function() {
